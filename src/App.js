86805fe51c8f9d28026ca21c11484c3fae89d037
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-01-22 13:28:31 +0200

    modified if-statement

--- src/App.js
+++ src/App.js
@@ -15,11 +15,15 @@ function App() {
     let burning = weight / 10;
     let lgrams = grams - (burning * time);
     let promiles = 0;
+
     if (gender === 'male') {
       promiles = lgrams / (weight * 0.7);
     }
-    else {
+    else if (gender === 'female') {
       promiles = lgrams / (weight * 0.6);
+    }
+    if (promiles < 0) {
+      promiles = 0;
     }
     setResult(promiles);
   }
@@ -31,20 +35,33 @@ function App() {
       <div>
         <div>
         <label>Weight </label>
-        <input type="number" value={weight} onChange={e => setWeight(e.target.value)} />
+        <input type="number" 
+        value={weight} 
+        onChange={e => setWeight(e.target.value)} />
       </div>
       <div>
         <label>Bottles </label>
-        <input type="number" value={bottles} onChange={e => setBottles(e.target.value)} />
+        <input type="number" 
+        value={bottles} 
+        onChange={e => setBottles(e.target.value)} />
         </div>
         <div>
           <label>Time </label>
-          <input type="number" value={time} onChange={e => setTime(e.target.value)} />
+          <input type="number" 
+          value={time} 
+          onChange={e => setTime(e.target.value)} />
           </div>
       <div>
         <label>Gender </label>
-        <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)} /><label>Male </label>
-        <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)}/><label>Female </label>
+        <input type="radio" 
+        name="gender" 
+        value="male" 
+        defaultChecked onChange={e => setGender(e.target.value)} />
+        <label>Male </label>
+        <input type="radio" 
+        name="gender" value="female" 
+        onChange={e => setGender(e.target.value)}/>
+        <label>Female </label>
       </div>
       </div>
     <div className='promile'>
